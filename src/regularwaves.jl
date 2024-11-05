7decdaf78490ae782d4e705de3b91d9b8039fdd9
mcpaprota <mapap@ibwpan.gda.pl>
2024-11-05 22:09:45 +0100

    finished implementing regular waves, formulas need checking

--- src/regularwaves.jl
+++ src/regularwaves.jl
@@ -88,7 +88,7 @@ Calculate free-surface elevation `η` at position `x` and time `t` for
 a regular standing wave of amplitude `a`, wave number `k`, and angular wave frequency `ω`.
 """
 function wave_profile_s(x::Real, t::Real, a::Real, k::Real, ω::Real)
-    η = a * cos(k * x - ω * t)
+    η = a * cos(k * x) * cos(ω * t)
     return η
 end
 
@@ -100,7 +100,7 @@ a regular standing wave of amplitude `a`, wave number `k`, and angular wave freq
 propagating at constant depth `d`.
 """
 function horizontal_velocity_s(x::Real, z::Real, t::Real, a::Real, k::Real, ω::Real, d::Real)
-    u = a * ω * cosh(k * (z + d)) / sinh(k * d) * cos(k * x - ω * t)
+    u = a * ω * cosh(k * (z + d)) / sinh(k * d) * sin(k * x) * sin(ω * t)
     return u
 end
 
@@ -112,7 +112,7 @@ a regular standing wave of amplitude `a`, wave number `k`, and angular wave freq
 propagating at constant depth `d`.
 """
 function vertical_velocity_s(x::Real, z::Real, t::Real, a::Real, k::Real, ω::Real, d::Real)
-    w = a * ω * sinh(k * (z + d)) / sinh(k * d) * sin(k * x - ω * t)
+    w = -a * ω * sinh(k * (z + d)) / sinh(k * d) * cos(k * x) * sin(ω * t)
     return w
 end
 
@@ -124,7 +124,7 @@ a regular standing wave of amplitude `a`, wave number `k`, and angular wave freq
 propagating at constant depth `d`.
 """
 function horizontal_displacement_s(x::Real, z::Real, t::Real, a::Real, k::Real, ω::Real, d::Real)
-    ξ = x - a * cosh(k * (z + d)) / sinh(k * d) * sin(k * x - ω * t)
+    ξ = x - a * cosh(k * (z + d)) / sinh(k * d) * sin(k * x) * cos(ω * t)
     return ξ
 end
 
@@ -136,7 +136,7 @@ a regular standing wave of amplitude `a`, wave number `k`, and angular wave freq
 propagating at constant depth `d`.
 """
 function vertical_displacement_s(x::Real, z::Real, t::Real, a::Real, k::Real, ω::Real, d::Real)
-    ζ = z + a * sinh(k * (z + d)) / sinh(k * d) * cos(k * x - ω * t)
+    ζ = z + a * sinh(k * (z + d)) / sinh(k * d) * cos(k * x) * cos(ω * t)
     return ζ
 end
 
@@ -157,16 +157,3 @@ function pressure_s(x::Real, z::Real, t::Real, a::Real, k::Real, ω::Real, d::Re
     end
     return p
 end
-
-  #= standing waves
-# linear wave theory - Airy waves
-η₀(x,t) = a*cos(k*x)*cos(ω*t) # free-surface elevation
-u₀(x,z,t) = a*ω*cosh(k*(z+d))/sinh(k*d)*sin(k*x)*sin(ω*t) # horizontal wave velocity component
-w₀(x,z,t) = -a*ω*sinh(k*(z+d))/sinh(k*d)*cos(k*x)*sin(ω*t) # vertical wave velocity component
-ζ(x,z,t) = x - a*cosh(k*(z+d))/sinh(k*d)*sin(k*x)*cos(ω*t) # horizontal water particle displacement
-ξ(x,z,t) = z + a*sinh(k*(z+d))/sinh(k*d)*cos(k*x)*cos(ω*t) # vertical water particle displacement
-p(x,z,t) = z > 0 ? ρ*g*(η₀(x,t) - z) : -ρ*g*z + ρ*g*a*cosh(k*(z+d))/sinh(k*d)*cos(k*x)*cos(ω*t)
-# second-order weakly-nonlinear correction - Stokes waves
-η₁(x,t) = a^2*k/8*(tanh(k*d)*(1+coth(k*d)^2)*cos(2*k*x)+coth(k*d)*(3*coth(k*d)^2-1)*cos(2*k*x)*cos(2*ω*t))  - a^2*k/4/sinh(2k*d)
-u₁(x,z,t) = 3*a^2*ω*k/8*cosh(2*k*(z+d))/sinh(k*d)^4*sin(2*k*x)*sin(2*ω*t)
-w₁(x,z,t) = -3*a^2*ω*k/8*sinh(2*k*(z+d))/sinh(k*d)^4*cos(2*k*x)*sin(2*ω*t) =#
