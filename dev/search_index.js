11a2b1c157210e95be11aac062469ef52d0b0441
Documenter.jl <documenter@juliadocs.github.io>
2024-11-05 13:35:04 +0000

    build based on 398c98e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = LinearWaves","category":"page"},{"location":"#LinearWaves","page":"Home","title":"LinearWaves","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for LinearWaves.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [LinearWaves]","category":"page"}]
+}
