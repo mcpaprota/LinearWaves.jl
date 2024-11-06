7ceb1d48abc7e663d2f0369abbfb7712273389c7
mcpaprota <mapap@ibwpan.gda.pl>
2024-11-06 11:19:20 +0100

    tests corrected

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,40 +9,46 @@ using Test
     ϵ = LinearWaves.tol
     g = LinearWaves.g
     ρ = LinearWaves.ρ
-    # Free surface tests
-    a, k, ω, d = 2, 1, 1, 1
-    # progressive wave
+    ## regular wave tests
+    a, k, d = 2, 1, 1
+    ω = sqrt(g * k * tanh(k * d))
+    ### progressive wave
     η(x, t) = wave_profile(x, t, a, k, ω)
     @test η(0, 0) == a
-    @test η(0, π) == -a
-    @test η(π / 2, π / 2) == a
+    @test η(0, π / ω) == -a
+    @test η(π / 2, π / 2ω) == a
     u(x, z, t) = horizontal_velocity(x, z, t, a, k, ω, d)
-    @test u(π / 2, 0, 0) < ϵ
-    @test u(0, 0, π / 2) < ϵ
+    @test abs(u(π / 2, 0, 0)) < ϵ
+    @test abs(u(0, 0, π / 2ω)) < ϵ
     w(x, z, t) = vertical_velocity(x, z, t, a, k, ω, d)
-    @test w(0, 0, 0) < ϵ
-    @test w(0, 0, π) < ϵ
-    @test w(π / 2, -1, 0) < ϵ
+    @test abs(w(0, 0, 0)) < ϵ
+    @test abs(w(0, 0, π / ω)) < ϵ
+    @test abs(w(π / 2, -1, 0)) < ϵ
     ξ(x, z, t) = horizontal_displacement(x, z, t, a, k, ω, d)
     ζ(x, z, t) = vertical_displacement(x, z, t, a, k, ω, d)
     @test ξ(0, 0, 0)^2 + ζ(0, 0, 0)^2 == a^2
     p(x, z, t) = pressure(x, z, t, a, k, ω, d)
-    @test p(π / 2, -d, 0) - p(3π / 2, -d, 0) < ϵ * g * ρ
-    # standing wave
+    @test abs(p(π / 2, -d, 0) - p(3π / 2, -d, 0)) < ϵ * g * ρ
+    ### standing wave
     η(x, t) = wave_profile_s(x, t, a, k, ω)
     @test η(0, 0) == a
-    @test η(0, π) == -a
-    @test η(0, 2π) == a
+    @test η(0, π / ω) == -a
+    @test η(0, 2π / ω) == a
     u(x, z, t) = horizontal_velocity_s(x, z, t, a, k, ω, d)
-    @test u(0, 0, 0) < ϵ
-    @test u(0, 0, π) < ϵ
+    @test abs(u(0, 0, 0)) < ϵ
+    @test abs(u(0, 0, π / ω)) < ϵ
     w(x, z, t) = vertical_velocity_s(x, z, t, a, k, ω, d)
-    @test w(π / 2, 0, 0) < ϵ
-    @test w(0, 0, π / 2) < ϵ
-    @test w(0, -1, 0) < ϵ
+    @test abs(w(π / 2, 0, 0)) < ϵ
+    @test abs(w(0, 0, π / ω)) < ϵ
+    @test abs(w(0, -1, 0)) < ϵ
     ξ(x, z, t) = horizontal_displacement_s(x, z, t, a, k, ω, d)
     ζ(x, z, t) = vertical_displacement_s(x, z, t, a, k, ω, d)
     @test ξ(0, 0, 0)^2 + ζ(0, 0, 0)^2 == a^2
     p(x, z, t) = pressure_s(x, z, t, a, k, ω, d)
-    @test p(π / 2, -d, 1) - p(3π / 2, -d, 1) < ϵ * g * ρ
+    @test abs(p(π / 2, -d, 1) - p(3π / 2, -d, 1)) < ϵ * g * ρ
+    ## wave processes
+    ω, d = 2π, 1
+    k = dispersion_relation(d, ω)
+    ### dispersion
+    @test abs(ω - sqrt(g * k * tanh(k * d))) < ϵ
 end
